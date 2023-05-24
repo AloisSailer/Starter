e3e21398b96b726bba8cf1bd7456069dac7d17f1
Alois Sailer <alois_sailer@yahoo.com.br>
2023-05-24 12:43:17 -0300

    aula 2 concluído

---
+++ scripts/intro.js
@@ -0,0 +1,145 @@
+// variaveis, tipos de dados
+
+// document.write("Hello")
+// console.log("Hello")
+// console.log(123)
+// --------------------------------
+
+// var myvar //undefined
+
+// myvar = "Alois"
+
+// document.write(myvar)
+// console.log(myvar)
+// -------------------------------
+
+// var myvar = "Al"
+// const myconst = "ois"
+
+// document.write(myvar + myconst)
+// console.log(myvar + myconst)
+// ---------------------------------
+
+// let mylet = "Sai"
+// const myconst = "ler"
+
+// document.write(mylet + myconst)
+// console.log(mylet + myconst)
+// --------------------------------
+
+// STRING
+
+// const s1 = "Isso é uma string  "
+// const s2 = 'Isso também é uma string  '
+// const s3 = `Isso é uma string também  `
+
+// document.write(s1)
+// console.log(s2)
+// document.write(s3)
+// --------------------------------------------------
+
+// NUMBER
+
+// const n1 = 1
+// const n2 = 12
+// const n3 = "2"
+
+// document.write(n1, n2)
+// console.log(n1 + n2)
+// console.log(n1 + n3)
+// ou
+// document.write(n3 + n1)
+//---------------------------------
+
+// BOOLEAN - TRUE - FALSE
+// ---------------------------------
+
+// OBJETO  - possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: '1,81cm',
+//   idade: 53,
+//   brasileiro: false,
+//   correr(){
+// document.write('Red Bull verleiht Flüüügel  ')
+//     return 'Red Bull verleiht Flüüügel  '
+//   }
+// }
+
+// document.write(pessoa)
+// console.log(pessoa)
+// console.log(pessoa.altura)
+// console.log(pessoa.idade)
+// console.log(pessoa.brasileiro)
+// console.log(pessoa.brasileiroo)
+
+// ou
+
+// document.write()
+// document.write(pessoa.correr())
+// -------------------------------------------
+
+// ARRAY - VETOR  uma coleção de dados
+
+// const colecaoDeBolinhas = ['blue', 'green', 5, {name:'Alois'}]
+
+// document.write(colecaoDeBolinhas)
+// document.write(colecaoDeBolinhas[0])
+// ou
+// document.write(colecaoDeBolinhas[3].name)
+
+// console.log(colecaoDeBolinhas)
+// ------------------------------------
+
+// FUNÇÕES  - funcionalidades, atalhos, reuso de código
+
+// registrar função
+// function sayMyName() {
+//   document.write("Alois")
+// esplicando: objeto.funcionalidade(string)
+// }
+// argumento ou parâmetro da função
+// executar a função
+// sayMyName()
+// sayMyName()
+// sayMyName()
+// ---
+
+// reuso de codigo:
+// function myCity(city) {
+//   document.write(city)
+// }
+
+// myCity("Graz")
+// myCity("Rio de Janeiro")
+// ------------------------------------
+
+// Condicionais
+
+// const notaFinal = 4
+
+// if(notaFinal < 5) {
+// // caminho 1
+//   document.write("Reprovado")
+// } else {
+// // caminho 2
+//   document.write("Aprovado")
+// }
+// ----------------------------------------------
+
+// LOOP  - repetições
+
+// for (i = 0; i < 5; i++) {
+//   // document.write("<h1>Alois</h1>")
+//   // ou
+//   document.write(`<p>${i}</p>`)
+// }
+
+// console.log("Hello")
+// document.write("Hello")
+
+// fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(
+//   function (res) {
+//     console.log(res)
+//   }
+// )
